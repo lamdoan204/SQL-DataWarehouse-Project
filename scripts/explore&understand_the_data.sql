063cea8f0e7d324b6f2409198cc4c992ef7413b4
lamdoan204 <lamvip2k4@gmail.com>
2025-11-28 00:17:48 +0700

    understanding the data

---
+++ scripts/explore&understand_the_data.sql
@@ -0,0 +1,11 @@
+-- select * from   bronze.crm_cust_info
+
+SELECT * from bronze.crm_prd_info
+
+-- SELECT * from bronze.crm_sales_details
+
+-- SELECT *from bronze.erp_cust_az_12
+
+-- SELECT * FROM bronze.erp_loc_a101
+
+SELECT * from  bronze.erp_px_cat_g1v2
